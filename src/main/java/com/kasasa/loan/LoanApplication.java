a77a0eed8ce38eabd50019c9dd4368e1f8ffd813
jose miguel angel monraz valencia <miguel.java20@gmail.com>
2021-05-10 09:57:56 -0500

    Applied lombok to the CustomException class

--- src/main/java/com/kasasa/loan/LoanApplication.java
+++ src/main/java/com/kasasa/loan/LoanApplication.java
@@ -6,51 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.io.IOException;
 import java.util.List;
 
 @SpringBootApplication
-public class LoanApplication implements CommandLineRunner {
-	@Autowired
-	UserRepository userRepository;
+public class LoanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args){
-		List<User> userList = userRepository.findAll();
-
-		if(userList.isEmpty()){
-			User user1 = new User();
-
-			user1.setUserName("test");
-			user1.setPassword("password");
-			user1.setRoles("ROLE_USER");
-			user1.setActive(true);
-
-			userRepository.save(user1);
-
-			User user2 = new User();
-
-			user2.setUserName("admin");
-			user2.setPassword("admin");
-			user2.setRoles("ROLE_USER, ROLE_ADMIN");
-			user2.setActive(true);
-
-			userRepository.save(user2);
-
-			User user3 = new User();
-
-			user3.setUserName("support");
-			user3.setPassword("support");
-			user3.setRoles("ROLE_SUPPORT");
-			user3.setActive(true);
-
-			userRepository.save(user3);
-
-		}
-	}
 }
--- src/main/java/com/kasasa/loan/exception/CustomException.java
+++ src/main/java/com/kasasa/loan/exception/CustomException.java
@@ -1,7 +1,11 @@
 package com.kasasa.loan.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import javax.validation.constraints.DecimalMax;
 import java.time.ZonedDateTime;
 
 /**
@@ -10,26 +14,10 @@ import java.time.ZonedDateTime;
  * when a 404, 400 or any exception happens which will be more useful
  * to the user
  */
+@Getter
+@AllArgsConstructor
 public class CustomException {
     private final String message;
     private final HttpStatus httpStatus;
     private final ZonedDateTime zonedDateTime;
-
-    public CustomException(String message, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
-        this.message = message;
-        this.httpStatus = httpStatus;
-        this.zonedDateTime = zonedDateTime;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
-
-    public ZonedDateTime getZonedDateTime() {
-        return zonedDateTime;
-    }
 }
